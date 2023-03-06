69e63a43f4e34944eebd773a76008753c475838c
Pandaxyz <98690785+Pandaxyzzz@users.noreply.github.com>
2023-03-06 22:52:46 +0100

    👾 (1.0.3) PEP 8 rules added

--- __init__.py
+++ __init__.py
@@ -1,109 +1,54 @@
-import os
-import sys
-from colorama import Fore as c
-import fade
+from __config__ import (__BANNER__,
+                        __ALERT__,
+                        __BAD__,
+                        __GOOD__)
+from __functions__ import Utils
+
 import requests
 import pystyle
 
+class Main(object):
+    def __init__(self) -> None:
+        self.Nano()
+        proxylist = pystyle.Write.Input(  #type: ignore
+            "⌈  Paste proxy path  ⌋ : ",
+            pystyle.Colors.blue_to_purple,
+            interval=0.0035
+        )
+        self.Nano()
+        self.hits = []
+        self.hits_file = 'hits.txt'
+        for proxy in open(proxylist, "r").readlines():
+            self.check(proxy)
+        for proxy in self.hits:
+            open(self.hits_file, 'a+').write(proxy + '\n')
+        print(f'{__GOOD__} Total proxies: {len(self.hits)}')
 
-# Clear function
-def clear():
-    """Clears console screen."""
-    if sys.platform == 'win32':
-        os.system('cls')
-    else:
-        os.system('clear')
+    def Nano(self) -> None:
+        Utils.clear()
+        Utils.title('Starting.')
+        print(__BANNER__)
 
-
-# Title function
-def title(title=None):
-    """Sets console title."""
-    try:
-        os.system(f'title NPC • {title}')
-    except:
-        os.system(f'title NPC')
-
-
-# Color class
-class color:
-    """Console color codes."""
-    green = c.GREEN
-    red = c.RED
-    blue = c.BLUE
-    yellow = c.LIGHTYELLOW_EX
-    reset = c.RESET
-
-
-# Banner string
-banner = """
-╔╗╔╔═╗╔═╗
-║║║╠═╝║      github/Pandaxyzzz
-╝╚╝╩  ╚═╝
-"""
-banner = fade.purpleblue(banner)
-
-# Console message codes
-good = f"[{color.green}!{color.reset}]"
-bad = f"[{color.red}*{color.reset}]"
-alert = f"[{color.yellow}>{color.reset}]"
-
-
-# Main function
-def main():
-    """Main function that checks proxies."""
-    clear()
-    title('Starting.')
-    print(banner)
-
-    proxylist = pystyle.Write.Input(
-        "⌈  Paste proxy path  ⌋ : ",
-        pystyle.Colors.blue_to_purple,
-        interval=0.0035
-    )
-
-    clear()
-    title('Checking')
-    print(banner)
-
-    hits = []
-    url = 'http://www.google.com'
-    hits_file = 'hits.txt'
-
-    with open(proxylist, 'r') as file:
-        proxies = file.readlines()
-
-        for proxy in proxies:
-            proxy = proxy.strip()
-
-            if not proxy.startswith('http://'):
-                proxy = f'http://{proxy}'
-
-            try:
-                response = requests.get(
-                    url,
-                    proxies={'http': proxy},
-                    timeout=10
-                )
-
-                if response.status_code == 200:
-                    global proxy_hit
-                    print(f'{good} {proxy}')
-                    hits.append(proxy)
-                    proxy_hit = proxy.split('http://')
-                    with open(hits_file, 'a') as f:
-                        f.write(f'{proxy_hit[1]}\n')
-            except:
-                print(f'{bad} {proxy}')
-
-    if hits:
-        with open(hits_file, 'w') as file:
-            for proxy in hits:
-                file.write(proxy + '\n')
-
-        print(f'{good} Total proxies: {len(hits)}')
-    else:
-        print(f'{bad} Total proxies: 0')
+    def check(self,
+              proxy: str) -> None:
+        url = 'http://www.google.com'
+        proxy = proxy.strip()
+        if not 'http://' in proxy:
+            proxy = f'http://{proxy}'
+        try:
+            response = requests.get(
+                url,
+                proxies={'http': proxy},
+                timeout=10
+            )
+            if response.status_code == 200:
+                print(f'{__GOOD__} {proxy}')
+                self.hits.append(proxy)
+                proxy_hit = proxy.split('http://')
+                open(self.hits_file, 'a+').write(f'{proxy_hit[1]}\n')
+        except:
+            print(f'{__BAD__} {proxy}')
 
 
 if __name__ == '__main__':
-    main()
+    Main()
