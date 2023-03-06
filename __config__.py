d2387d8fd6ab4cec5333b9878bf6f000709cddb6
Pandaxyz <98690785+Pandaxyzzz@users.noreply.github.com>
2023-03-06 22:53:02 +0100

    Add files via upload

---
+++ __config__.py
@@ -0,0 +1,13 @@
+from colorama import Fore
+import enum
+import fade
+
+__BANNER__ = fade.purpleblue("""
+╔╗╔╔═╗╔═╗
+║║║╠═╝║      github/Pandaxyzzz
+╝╚╝╩  ╚═╝
+""")
+
+__GOOD__ = f"[{Fore.GREEN}!{Fore.RESET}]"
+__BAD__ = f"[{Fore.RED}*{Fore.RESET}]"
+__ALERT__ = f"[{Fore.RESET}>{Fore.RESET}]"
---
+++ __functions__.py
@@ -0,0 +1,12 @@
+import os
+
+class Utils(object):
+    @staticmethod
+    def clear() -> None:
+        os.system("cls||clear")
+    @staticmethod
+    def title(title: str) -> None:
+        if len(title) > 0:
+            os.system(f'title NPC • {title}')
+        else:
+            os.system(f'title NPC')
